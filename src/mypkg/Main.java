d980f81450ef3302494340d4139bf5364a727d6e
profwpollock <pollock@acm.org>
2015-01-16 00:01:44 -0500

    Create Main.java

---
+++ src/mypkg/Main.java
@@ -0,0 +1,26 @@
+// Git collaborative COP-2805 (Java II) project.
+
+package mypkg;
+
+/**
+ * COP-2805 - Project 2: Using Git and GitHub
+ * @author Wayne Pollock
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		int counter = 0;
+
+		// (1) Add your own output below that includes your name.
+		// (2) Place "counter++;" command after your contribution, to keep count.
+		// (3) Keep the last println statement at the very end.
+
+		System.out.println( "Howdy!  My name is Wayne Pollock." );
+		counter++;
+
+
+
+		System.out.println( "Number of people who edited this file: "
+				+ counter );
+	}
+}
