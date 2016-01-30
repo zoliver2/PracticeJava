fbdd09810b81755ba97e42bc1a5f0b1d9e857a1a
Kevin Lane Kirkfield II <klkirkfield2@hotmail.com>
2016-01-30 13:46:36 -0500

    Add Kevin Kirkfield sumbission
    This commit includes my submission. I ran the source code formatter in
    Eclipse because the indents didn't match from the previous student's
    submission.

--- src/mypkg/Main.java
+++ src/mypkg/Main.java
@@ -4,6 +4,7 @@ package mypkg;
 
 /**
  * COP-2805 - Project 2: Using Git and GitHub
+ * 
  * @author Wayne Pollock
  */
 public class Main {
@@ -12,19 +13,21 @@ public class Main {
 		int counter = 0;
 
 		// (1) Add your own output below that includes your name.
-		// (2) Place "counter++;" command after your contribution, to keep count.
+		// (2) Place "counter++;" command after your contribution, to keep
+		// count.
 		// (3) Keep the last println statement at the very end.
 
-		System.out.println( "Howdy!  My name is Wayne Pollock." );
+		System.out.println("Howdy!  My name is Wayne Pollock.");
 		counter++;
 
-               // Hello statment
-               // changed from what was here before..
-                System.out.println("Hello! its me, Thomas Schlicher;" + 
-                                    " Starbucks coffee + Java = GREATNESS!");
-                counter++;
-                
-		System.out.println( "Number of people who edited this file: "
-				+ counter );
+		// Hello statment
+		// changed from what was here before..
+		System.out.println("Hello! its me, Thomas Schlicher;" + " Starbucks coffee + Java = GREATNESS!");
+		counter++;
+
+		System.out.println("Hey, it's Kevin Kirkfield.");
+		counter++;
+
+		System.out.println("Number of people who edited this file: " + counter);
 	}
 }
