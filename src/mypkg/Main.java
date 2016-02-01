93daf4e0f34d51716a584a8f91696af9220d382d
jfigueroa <jfigueroa@jfigueroa-PC>
2016-01-31 21:36:42 -0500

    Added code that introduces myself - Joshua Figueroa Roldan

--- src/mypkg/Main.java
+++ src/mypkg/Main.java
@@ -27,7 +27,10 @@ public class Main {
 
 		System.out.println("Hey, it's Kevin Kirkfield.");
 		counter++;
-
+                
+                System.out.println("Hello, this is Joshua Figueroa Roldan! Is this thing on?");
+                counter++;
+                
 		System.out.println("Number of people who edited this file: " + counter);
 	}
 }
